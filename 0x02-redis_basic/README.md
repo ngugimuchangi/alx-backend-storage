7263eabce3c2aa6173ef6472133582037f2bfb27
ngugimuchangi <tech.curious14@gmail.com>
2023-03-30 12:07:37 +0300

    tasks: 1 - writing to, reading and recoverign values from redis

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,8 @@
+# 0x02. Redis basic
+
+## About
+- Redis basics
+- Redis for simple caching
+- Creating redis clients with `redis-py` library
+
+## Tasks
---
+++ 0x02-redis_basic/exercies.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+""" Redis client module
+"""
+import redis
+from uuid import uuid4
+from functools import wraps
+from typing import Any, Callable, List, Union
+
+
+def count_calls(method: Callable) -> Callable:
+    """ Decorator for Cache class methods
+    """
+    def wrapper(*args: List) -> Callable:
+        pass
+
+
+class Cache:
+    """ Caching class
+    """
+    def __init__(self) -> None:
+        """ Initialize new cache object
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes,  int,  float]) -> str:
+        """ Stores data in redis with randomly generated key
+        """
+        key = str(uuid4())
+        client = self._redis
+        client.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Callable) -> Any:
+        """ Gets key's value from redis and converts
+            result byte  into correct data type
+        """
+        client = self._redis
+        value = client.get(key)
+        if fn is int:
+            return self.get_int(value)
+        if fn is str:
+            return self.get_str(value)
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, data: bytes) -> str:
+        """ Converts bytes to string
+        """
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """ Converts bytes to integers
+        """
+        return int(data)
