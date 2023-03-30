41c38544f3a6582761df918c35210898bd15fd13
ngugimuchangi <tech.curious14@gmail.com>
2023-03-30 18:46:09 +0300

    fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,10 +4,8 @@ Caching request module
 """
 import redis
 import requests
-from typing import Callable
 from functools import wraps
-
-client = redis.Redis()
+from typing import Callable
 
 
 def track_get_page(fn: Callable) -> Callable:
@@ -19,13 +17,13 @@ def track_get_page(fn: Callable) -> Callable:
             - check whether a url's data is cached
             - tracks how many times get_page is called
         """
-        client.incr(f'count:{url}')
-        client.incr(f'count:{{url}}')
+        client = redis.Redis()
         cached_data = client.get(f'{url}')
         if cached_data:
             return cached_data.decode('utf-8')
         response = fn(url)
-        client.set(f'{url}', response, 10)
+        client.incr(f'count:{url}')
+        client.set(f'{url}', response, 10.0)
         return response
     return wrapper
 
@@ -36,5 +34,3 @@ def get_page(url: str) -> str:
     """
     response = requests.get(url)
     return response.text
-
-get_page('http://slowwly.robertomurray.co.uk')
