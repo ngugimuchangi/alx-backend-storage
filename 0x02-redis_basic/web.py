c4ff2d39a5262a7a992b32acb1553ed1266f3e60
ngugimuchangi <tech.curious14@gmail.com>
2023-03-30 18:32:32 +0300

    fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -19,13 +19,12 @@ def track_get_page(fn: Callable) -> Callable:
             - check whether a url's data is cached
             - tracks how many times get_page is called
         """
-        cached_data = client.get(f'cached:{url}')
+        cached_data = client.get(f'{url}')
         if cached_data:
             return cached_data.decode('utf-8')
         response = fn(url)
-        client.incr('count:{url}')
-        # client.set(f'cached:{url}', response)
-        # client.expire(f'cached:{url}', 10)
+        client.incr(f'count:{url}')
+        client.set(f'{url}', response, 10)
         return response
     return wrapper
 
@@ -36,3 +35,5 @@ def get_page(url: str) -> str:
     """
     response = requests.get(url)
     return response.text
+
+get_page()
