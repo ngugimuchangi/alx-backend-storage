fc8edbb3a2d7f7ca6b9b9f41556a7f9eb66bbd87
ngugimuchangi <tech.curious14@gmail.com>
2023-04-02 06:58:54 +0300

    updating documents with pymongo

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+PyMongo operations - update
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Updates topics field with given topics for
+    documents whose name field matches given name
+    """
+    mongo_collection.update_many({'name': name}, {'topics': topics})
