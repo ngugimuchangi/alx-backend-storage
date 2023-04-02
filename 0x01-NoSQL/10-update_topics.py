58f2716940c5eae90f5b647a6c453e39cf07d880
ngugimuchangi <tech.curious14@gmail.com>
2023-04-02 07:03:56 +0300

    fixed update operation

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -9,4 +9,5 @@ def update_topics(mongo_collection, name, topics):
     Updates topics field with given topics for
     documents whose name field matches given name
     """
-    mongo_collection.update_many({'name': name}, {'topics': topics})
+    mongo_collection.update_many({'name': name},
+                                 {'$set': {'topics': topics}})
