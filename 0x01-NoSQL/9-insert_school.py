02ef1efdad26355c1e7d3c619b6d051d53a103fc
ngugimuchangi <tech.curious14@gmail.com>
2023-04-02 06:44:59 +0300

    inserting new documents with pymongo

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+PyMongo Operations
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ Inserts a new document in a mongodb collection
+        based on on kwargs
+    """
+    return mongo_collection.insert(kwargs).inserted_id
