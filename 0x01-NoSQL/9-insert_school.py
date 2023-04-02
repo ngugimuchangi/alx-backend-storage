9028e02464c237ddaf1d52b581f8cdd1b393db3c
ngugimuchangi <tech.curious14@gmail.com>
2023-04-02 06:47:00 +0300

    fixed method

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -8,4 +8,4 @@ def insert_school(mongo_collection, **kwargs):
     """ Inserts a new document in a mongodb collection
         based on on kwargs
     """
-    return mongo_collection.insert(**kwargs).inserted_id
+    return mongo_collection.insert_one(kwargs).inserted_id
