ae336d3d04ecbb12bf89fe9e9d51f5f67663a777
ngugimuchangi <tech.curious14@gmail.com>
2023-04-02 07:32:05 +0300

    fixed function name

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,8 +4,8 @@ PyMongo operations: matching data in list
 """
 
 
-def school_by_topic(mongo_collection, topic):
+def schools_by_topic(mongo_collection, topic):
     """
     Finds a list of school having a specific topic
     """
-    return mongo_collection.find({'topics': {'$in': [topic]}})
+    return mongo_collection.find({'topics': topic})
