65d5437a725ed9ce822fe2f60d649a3ac4c59f37
ngugimuchangi <tech.curious14@gmail.com>
2023-05-07 11:14:09 +0300

    changed query to fix date value

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Selects bands with Glam Rock as their
 -- style and ranks them by their longevity
 SELECT band_name,
-IFNULL(split, year(current_date())) - formed AS lifespan
+IFNULL(split, 2020) - formed AS lifespan
 FROM metal_bands
 WHERE style like '%Glam rock%'
 ORDER BY lifespan DESC;
