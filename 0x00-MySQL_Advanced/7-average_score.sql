e56728e4e53027d36a5df6aba3c540756422b2dd
ngugimuchangi <tech.curious14@gmail.com>
2023-05-07 17:15:40 +0300

    fixed column-variable conflict

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,8 +4,7 @@
 DELIMITER $
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    SET @average_score = (SELECT AVG(score) AS average FROM corrections
-    WHERE user_id = user_id);
-    UPDATE users SET average_score = @average_score WHERE id=user_id;
+    SELECT AVG (score) INTO @average_score FROM corrections WHERE corrections.user_id = user_id;
+    UPDATE users SET users.average_score = @average_score WHERE id = user_id;
 END$
 DELIMITER ;
